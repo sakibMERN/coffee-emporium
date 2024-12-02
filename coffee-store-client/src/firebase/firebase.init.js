b70f1ccc599c0142f848bf5247e04268983afa29
sakibMERN <sakib.sr57@gmail.com>
2024-12-02 18:47:21 +0600

    add .env.local for secure my firebase auth

--- coffee-store-client/src/firebase/firebase.init.js
+++ coffee-store-client/src/firebase/firebase.init.js
@@ -5,13 +5,21 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA86KOSbU_4zab5WE7b7fFT0q5NWl5s3uI",
+//   authDomain: "coffee-store-f3d60.firebaseapp.com",
+//   projectId: "coffee-store-f3d60",
+//   storageBucket: "coffee-store-f3d60.firebasestorage.app",
+//   messagingSenderId: "238357798619",
+//   appId: "1:238357798619:web:6edec43dab97666d7b36fe"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyA86KOSbU_4zab5WE7b7fFT0q5NWl5s3uI",
-  authDomain: "coffee-store-f3d60.firebaseapp.com",
-  projectId: "coffee-store-f3d60",
-  storageBucket: "coffee-store-f3d60.firebasestorage.app",
-  messagingSenderId: "238357798619",
-  appId: "1:238357798619:web:6edec43dab97666d7b36fe"
+  apiKey:import.meta.env.VITE_apiKey ,
+  authDomain:import.meta.env.VITE_authDomain ,
+  projectId:import.meta.env.VITE_projectId ,
+  storageBucket:import.meta.env.VITE_storageBucket ,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId ,
+  appId:import.meta.env.VITE_appId 
 };
 
 // Initialize Firebase
